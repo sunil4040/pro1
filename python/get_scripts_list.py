763f96da8d0dc7b048de132a87ee974d051fcfab
sunil4040 <sunil4040@yahoo.com>
2017-02-10 04:21:49 +0000

    Update get_scripts_list.py

--- python/get_scripts_list.py
+++ python/get_scripts_list.py
@@ -11,5 +11,10 @@ def get_script_symbol(scrip, scripts_list):
     symbol = (scripts_list.loc[scripts_list.NAME_OF_COMPANY.str.startswith(scrip.upper())]).SYMBOL
     if len(symbol) == 1:
         return symbol.iloc[0]
+    elif len(symbol) == 0:
+        symbol = (scripts_list.loc[scripts_list.NAME_OF_COMPANY.str.startswith('THE ' + scrip.upper())]).SYMBOL
+        if len(symbol) == 1:
+            return symbol.iloc[0]
+        return 'NA'
     else:
         return 'NA'
