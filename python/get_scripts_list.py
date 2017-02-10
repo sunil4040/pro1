7cad24825f513b1bdfeb035d11950199fd88df11
sunil4040 <sunil4040@yahoo.com>
2017-02-10 14:12:18 +0000

    Update get_scripts_list.py

--- python/get_scripts_list.py
+++ python/get_scripts_list.py
@@ -5,7 +5,10 @@ script_list = pd.DataFrame()
 def get_scripts_list():
     column_names = ['SYMBOL', 'NAME_OF_COMPANY', 'SERIES', 'DATE_OF_LISTING', 'PAID_UP_VALUE', 'MARKET_LOT', 'ISIN_NUMBER',
                'FACE_VALUE']
-    return pd.read_csv('C:/data/config/EQUITY_L.csv', encoding='cp1252', header=0, names=column_names, usecols=['SYMBOL', 'NAME_OF_COMPANY'])
+    scripts_list = pd.read_csv('C:/data/config/EQUITY_L.csv', encoding='cp1252', header=0, names=column_names,
+                usecols=['SYMBOL', 'NAME_OF_COMPANY'])
+    scripts_list.NAME_OF_COMPANY = scripts_list.NAME_OF_COMPANY.str.upper()
+    return scripts_list
 
 def get_script_symbol(scrip, scripts_list):
     symbol = (scripts_list.loc[scripts_list.NAME_OF_COMPANY.str.startswith(scrip.upper())]).SYMBOL
