35f175e0d6771e122bc69dfbe5fc48b6d041e773
Sunil <sunil4040@yahoo.com>
2017-02-12 09:12:12 +0000

    more

--- R/test.R
+++ R/test.R
@@ -41,4 +41,5 @@ recs_counts <- recs_not_na %>% group_by(SYMBOL) %>% summarize(count=n())
 # Testing to setup
 get_scrpits_list('NSE')
 
+recs_wo_url <- recs[,-11]
 aggregate(PERF_TO_DATE ~ RECOMMENDER, recs_wo_url[grepl('2017$', recs_wo_url$REC_DATE), ], max)
