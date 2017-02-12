e13c61bb233c27c4d04785396c05d82382540a62
Sunil <sunil4040@yahoo.com>
2017-02-12 02:47:17 +0000

    py_loc

--- R/process_bhav_archives.R
+++ R/process_bhav_archives.R
@@ -1,8 +1,8 @@
 process_bhav_archives <- function()
 {
-  if(file.exists('C:/data/processed/bhav_cons.csv'))
+  if(file.exists('C:/data/bhav_cons/bhav_cons.csv'))
   {
-    file.remove('C:/data/processed/bhav_cons.csv')
+    file.remove('C:/data/bhav_cons/bhav_cons.csv')
   }
   bhav_archive_location <- "C:/data/archive/"
   zip_files <- list.files(bhav_archive_location, pattern = "*.csv.zip", full.names = TRUE)
